71aafef5523c52b54c13fcfd719390c3e23e5cf2
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2023-04-21 00:59:19 +0200

    make Message generic, improve logic, add missing docs

--- src/main/java/cloud/grabsky/bedrock/components/Message.java
+++ src/main/java/cloud/grabsky/bedrock/components/Message.java
@@ -41,21 +41,33 @@ import static java.lang.String.valueOf;
 import static net.kyori.adventure.text.minimessage.tag.resolver.Placeholder.component;
 import static net.kyori.adventure.text.minimessage.tag.resolver.Placeholder.unparsed;
 
+/**
+ * {@link Message} class simplifies creation and sending of context-dependent messages of various types.
+ */
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public sealed class Message<T> permits Message.StringMessage, Message.ComponentMessage {
+public abstract sealed class Message<T> permits Message.StringMessage, Message.ComponentMessage {
 
+    /**
+     * Returns plain {@link String} currently held by this {@link Message} instance.
+     */
     @Getter(AccessLevel.PUBLIC)
     protected @Nullable T message;
 
-    public static StringMessage of(final @Nullable String message) {
+    /**
+     * Sends message contained by (this) {@link Message} instance to provided {@link Audience}.
+     */
+    public abstract void send(final @NotNull Audience audience);
+
+    /**
+     * Sends message contained by (this) {@link Message} instance to all players.
+     */
+    public abstract void broadcast();
+
+
+    public static Message<String> of(final @Nullable String message) {
         return new StringMessage(message);
     }
 
-    public static ComponentMessage of(final @Nullable Component message) {
-        return new ComponentMessage(message);
-    }
-
-    // For (String) messages that requires deserialization.
     public static final class StringMessage extends Message<String> {
 
         private final TagResolver.Builder resolverBuilder = TagResolver.builder();
@@ -67,11 +79,11 @@ public sealed class Message<T> permits Message.StringMessage, Message.ComponentM
         /**
          * Replaces all occurences of <b>{@code target}</b> with <b>{@code to}</b>.
          */
-        public StringMessage replace(final @NotNull String target, final @Nullable String replacement) {
+        public StringMessage replace(final @NotNull String target, final @NotNull String replacement) {
             if (message == null)
                 return this;
             // ...
-            this.message = this.message.replace(target, (replacement != null) ? replacement : "");
+            this.message = message.replace(target, replacement);
             return this;
         }
 
@@ -101,15 +113,6 @@ public sealed class Message<T> permits Message.StringMessage, Message.ComponentM
         }
 
         /**
-         * Creates and adds {@link Placeholder} of <b>{@code name}</b> to be replaced with result of {@link Key#asString Key#asString} called on <b>{@code value}</b>.
-         */
-        // TO-DO: Replace with enhanced swtich once it leaves preview.
-        public StringMessage placeholder(final @NotNull String name, final @NotNull Key value) {
-            resolverBuilder.resolver(unparsed(name, value.asString()));
-            return this;
-        }
-
-        /**
          * Creates and adds {@link Placeholder} of <b>{@code name}</b> to be replaced with result of {@link Player#getName Player#getName} called on <b>{@code value}</b>.
          */
         // TO-DO: Replace with enhanced swtich once it leaves preview.
@@ -121,6 +124,7 @@ public sealed class Message<T> permits Message.StringMessage, Message.ComponentM
         /**
          * Creates and adds {@link Placeholder} of <b>{@code name}</b> to be replaced with result of {@link Object#toString Object#toString} called on <b>{@code value}</b>.
          */
+        // TO-DO: Replace with enhanced swtich once it leaves preview.
         public StringMessage placeholder(final @NotNull String name, final @NotNull Object value) {
             resolverBuilder.resolver(unparsed(name, valueOf(value)));
             return this;
@@ -134,62 +138,62 @@ public sealed class Message<T> permits Message.StringMessage, Message.ComponentM
             return this;
         }
 
-        /**
-         * Parses using {@link GlobalComponentSerializer}, and sends this {@link Message} to {@link Player}. Empty or {@code null} messages are not sent.
-         */
-        public void send(final @NotNull Audience sender) {
-            if (message == null || "".equals(message) == true)
+        @Override
+        public void send(final @NotNull Audience audience) {
+            // Ignoring empty/blank messages.
+            if (message == null || ("").equals(message) == true)
                 return;
-            // ...
+            // Parsing using MiniMessage instance provided by GlobalComponentSerializer.
             final Component component = GlobalComponentSerializer.get().deserialize(message, resolverBuilder.build());
-            // ...
-            if (component == Component.empty())
+            // Ignoring empty/blank messages.
+            if (Component.empty().equals(component) == true)
                 return;
-            // ...
-            sender.sendMessage(component);
+            // Sending message to the provided audience.
+            audience.sendMessage(component);
         }
 
-        /**
-         * Parses using {@link GlobalComponentSerializer}, and sends this {@link Message} to all players. Empty or {@code null} messages are not sent.
-         */
+        @Override
         public void broadcast() {
-            if (message == null || "".equals(message) == true)
+            // Ignoring empty/blank messages.
+            if (message == null || ("").equals(message) == true)
                 return;
-            // ...
+            // Parsing using MiniMessage instance provided by GlobalComponentSerializer.
             final Component component = GlobalComponentSerializer.get().deserialize(message, resolverBuilder.build());
-            // ...
-            if (component == Component.empty())
+            // Ignoring empty/blank messages.
+            if (Component.empty().equals(component) == true)
                 return;
-            // ...
+            // Broadcasting message to the server.
             Bukkit.broadcast(component);
         }
 
     }
 
-    // For already deserialized (Component) messages.
+
+    public static Message<Component> of(final @Nullable Component message) {
+        return new ComponentMessage(message);
+    }
+
     public static final class ComponentMessage extends Message<Component> {
 
-        public ComponentMessage(final @Nullable Component message) {
+        private ComponentMessage(final @Nullable Component message) {
             super(message);
         }
 
-        /**
-         * Parses using {@link GlobalComponentSerializer}, and sends this {@link Message} to {@link Player}. Empty or {@code null} messages are not sent.
-         */
-        public void send(final @NotNull Audience sender) {
-            if (message == null || message == Component.empty())
+        @Override
+        public void send(final @NotNull Audience audience) {
+            // Ignoring empty/blank messages.
+            if (message == null || Component.empty().equals(message) == true)
                 return;
-            // ...
-            sender.sendMessage(message);
+            // Sending message to the provided audience.
+            audience.sendMessage(message);
         }
 
-        /**
-         * Parses using {@link GlobalComponentSerializer}, and sends this {@link Message} to all players. Empty or {@code null} messages are not sent.
-         */
+        @Override
         public void broadcast() {
-            if (message == null || message == Component.empty())
+            // Ignoring empty/blank messages.
+            if (message == null || Component.empty().equals(message) == true)
                 return;
-            // ...
+            // Broadcasting message to the server.
             Bukkit.broadcast(message);
         }
 
