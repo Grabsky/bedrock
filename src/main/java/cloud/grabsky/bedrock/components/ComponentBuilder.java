52ad945872667a5478c1a53fce015eb6753ea272
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2023-08-14 20:55:34 +0200

    add ComponentBuilder

---
+++ src/main/java/cloud/grabsky/bedrock/components/ComponentBuilder.java
@@ -0,0 +1,81 @@
+package cloud.grabsky.bedrock.components;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.format.TextFormat;
+import net.kyori.adventure.translation.Translatable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.stream.Stream;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ComponentBuilder {
+
+    private TextComponent.Builder internal;
+
+    public static final Component EMPTY = Component.empty();
+    public static final Component EMPTY_NO_ITALIC = Component.empty().decoration(TextDecoration.ITALIC, false);
+
+    public static ComponentBuilder of(final @NotNull String text, final @NotNull TextFormat @Nullable ... format) {
+        // Returning new instance of ComponentBuilder with no formatting.
+        if (format == null)
+            return new ComponentBuilder(Component.text().content(text));
+        // Applying last specified text color.
+        final @Nullable TextColor color = (TextColor) Stream.of(format).filter(e -> e instanceof TextColor).reduce((first, second) -> second).orElse(null);
+        // Applying all text decorations.
+        final TextDecoration[] decorations = Stream.of(format).filter(e -> e instanceof TextDecoration).toArray(TextDecoration[]::new);
+        // Returning new instance of ComponentBuilder.
+        return new ComponentBuilder(Component.text().content(text).decorate(decorations).color(color));
+    }
+
+    public static ComponentBuilder of(final @NotNull Component component) {
+        // Returning new instance of ComponentBuilder.
+        return new ComponentBuilder(Component.text().append(component));
+    }
+
+    public ComponentBuilder append(final @NotNull String text, final @NotNull TextFormat @Nullable ... format) {
+        internal.append(of(text, format).build());
+        // Returning this instance.
+        return this;
+    }
+
+    public ComponentBuilder append(final @NotNull Component component) {
+        internal.append(component);
+        // Returning this instance.
+        return this;
+    }
+
+    public ComponentBuilder appendTranslation(final @NotNull Translatable translatable) {
+        internal.append(Component.translatable(translatable.translationKey()));
+        // Returning this instance.
+        return this;
+    }
+
+    public ComponentBuilder appendTranslation(final @NotNull String translationKey) {
+        internal.append(Component.translatable(translationKey));
+        // Returning this instance.
+        return this;
+    }
+
+    public ComponentBuilder color(final @NotNull TextColor color) {
+        internal.color(color);
+        // Returning this instance.
+        return this;
+    }
+
+    public ComponentBuilder decoration(final @NotNull TextDecoration color, final boolean state) {
+        internal.decoration(color, state);
+        // Returning this instance.
+        return this;
+    }
+
+    public Component build() {
+        return internal.build();
+    }
+
+}
